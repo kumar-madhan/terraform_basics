801c6b3caa7a7c45ea06134a82c9268b3a24ff63
kumar-madhan <madhankumar150598@gmail.com>
2024-03-16 13:32:55 +0000

    first

--- main.tf
+++ main.tf
@@ -6,17 +6,17 @@ resource "digitalocean_tag" "droplet_1" {
 
 # Configure the DigitalOcean provider
 provider "digitalocean" {
-  token = "dop_v1_981f9b04529cb9329a1b05c15976a639b9502682e94cb654a823866813f13644"
+  token = "dop_v1_7e075da2c5d123161362c6de367f60616518e5cde4726453ff9861182a849879"
 }
 resource "digitalocean_ssh_key" "droplet_1" {
   name       = "Terraform-SSH"
-  public_key = file("/root/.ssh/id_rsa.pub")
+  public_key = file("/home/admin/.ssh/id_rsa.pub")
 }
 # Define a DigitalOcean Droplet
 resource "digitalocean_droplet" "droplet_1" {
-  name   = "controller-node-${formatdate("HHMM", timestamp())}"
+  name   = "base-machine"
   region = "blr1"
-  size   = "s-2vcpu-2gb"
+  size   = "s-4vcpu-8gb"
   image  = "centos-stream-8-x64"
   tags   = [digitalocean_tag.droplet_1.id] 
   
